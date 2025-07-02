92d5f9fc673cd767c5ff75a2e77b94e1a094a072
Sun Sreng <sun_sreng@hotmail.com>
2025-07-02 21:15:31 +0700

    fix: Correct version number in deno.json and add return type to cn function

--- lib/cn.ts
+++ lib/cn.ts
@@ -1,6 +1,6 @@
 import { twMerge } from "tailwind-merge";
 import { clsx, type ClassValue } from "./clsx.ts";
 
-export const cn = (...inputs: ClassValue[]) => {
+export const cn = (...inputs: ClassValue[]): string => {
   return twMerge(clsx(...inputs));
 };
